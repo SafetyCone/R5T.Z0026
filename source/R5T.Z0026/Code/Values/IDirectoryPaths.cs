8c5ebf2d6295e4bdf2fd966c3c7baccc6bb06cfe
davidcoats <david@getsafetycone.com>
2022-12-27 12:48:55 -0800

    Interim changes.

--- source/R5T.Z0026/Code/Values/IDirectoryPaths.cs
+++ source/R5T.Z0026/Code/Values/IDirectoryPaths.cs
@@ -8,7 +8,13 @@ namespace R5T.Z0026
     [ValuesMarker]
     public partial interface IDirectoryPaths : IValuesMarker
     {
+        /// <summary>
+        /// ../David/Dropbox/.../Rivet/Shared/Data/Output
+        /// </summary>
         public string CloudSharedOutputDirectoryPath => @"C:\Users\David\Dropbox\Organizations\Rivet\Shared\Data\Output\";
+        /// <summary>
+        /// ../David/Dropbox/.../Rivet/Shared/Data/Instances
+        /// </summary>
         public string CloudSharedInstancesDirectoryPath => @"C:\Users\David\Dropbox\Organizations\Rivet\Shared\Data\Instances";
     }
 }
