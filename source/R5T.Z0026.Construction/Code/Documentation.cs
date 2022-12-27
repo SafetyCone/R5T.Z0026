a60558e9cddbcf76bac86a4f0de2906322ba5937
davidcoats <david@getsafetycone.com>
2022-12-26 16:11:05 -0800

    Initial commit.

---
+++ source/R5T.Z0026.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0026.Construction
+{
+	/// <summary>
+	/// Construction console project for the R5T.Z0026 library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.Z0026/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0026
+{
+	/// <summary>
+	/// Cloud, shared, directory paths.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
